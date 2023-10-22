892777d1fb7ab02dbb7bfa859716c2117f549b6c
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2023-10-22 15:20:51 +0500

    Service Extension added

---
+++ Zeemlin.Api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Org.BouncyCastle.Asn1.X509.Qualified;
+using System.Runtime.CompilerServices;
+using Zeemlin.Data.IRepositries;
+using Zeemlin.Data.Repositories;
+using Zeemlin.Service.Interfaces;
+using Zeemlin.Service.Services;
+
+namespace Zeemlin.Api.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void AddZeemlinService(this IServiceCollection services)
+    {
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+    }
+}
