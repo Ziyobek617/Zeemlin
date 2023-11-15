27c2f1ba55c0ccb22aecdb0fefef21eb0afc8d14
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2023-11-15 09:58:19 +0500

    Logger added for MiddleWare

--- Zeemlin.Api/Middlewares/ExceptionHandlerMiddleWare.cs
+++ Zeemlin.Api/Middlewares/ExceptionHandlerMiddleWare.cs
@@ -6,10 +6,12 @@ namespace Zeemlin.Api.Middlewares;
 public class ExceptionHandlerMiddleWare
 {
     private readonly RequestDelegate next;
+    private readonly ILogger<ExceptionHandlerMiddleWare> logger;
 
-    public ExceptionHandlerMiddleWare(RequestDelegate next)
+    public ExceptionHandlerMiddleWare(RequestDelegate next, ILogger<ExceptionHandlerMiddleWare> logger)
     {
         this.next = next;
+        this.logger = logger;
     }
 
     public async Task Invoke(HttpContext context)
@@ -20,6 +22,7 @@ public class ExceptionHandlerMiddleWare
         }
         catch (ZeemlinException ex)
         {
+            this.logger.LogError(ex.Message);
             context.Response.StatusCode = ex.StatusCode;
             await context.Response.WriteAsJsonAsync(new Response
             {
@@ -29,6 +32,7 @@ public class ExceptionHandlerMiddleWare
         }
         catch (Exception ex)
         {
+            this.logger.LogError($"{ex}\n\n");
             context.Response.StatusCode = 500;
             await context.Response.WriteAsJsonAsync(new Response
             {
