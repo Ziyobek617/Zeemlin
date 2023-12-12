5307e56db81e24d52eb0415249a200d08f31b6ca
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2023-12-12 11:51:57 +0500

    Add: AuthController, GroupPermissionsController,GroupRolesController

---
+++ Zeemlin.Api/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Zeemlin.Service.DTOs.User;
+using Zeemlin.Service.Interfaces;
+
+namespace Zeemlin.Api.Controllers;
+
+public class AuthController : BaseController
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("autenticate")]
+    public async Task<IActionResult> PostAsync(LoginDto dto)
+    {
+        var token = await _authService.AuthenticateAsync(dto);
+
+        return Ok(token);
+    }
+}
---
+++ Zeemlin.Api/Controllers/GroupPermissionsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Zeemlin.Domain.Enums;
+using Zeemlin.Service.Configurations;
+using Zeemlin.Service.Interfaces;
+
+namespace Zeemlin.Api.Controllers;
+
+public class GroupPermissionsController : BaseController
+{
+
+    private readonly IGroupPermissionService _groupPermissionService;
+
+    public GroupPermissionsController(IGroupPermissionService groupPermissionService)
+    {
+        this._groupPermissionService = groupPermissionService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync([FromBody] GroupPermissionType type)
+        => Ok(await this._groupPermissionService.CreateAsync(type));
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync([FromQuery] PaginationParams @params)
+        => Ok(await _groupPermissionService.RetrieveAllAsync(@params));
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetAsync([FromRoute(Name = "id")] long id)
+        => Ok(await this._groupPermissionService.RetrieveByIdAsync(id));
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutAsync([FromRoute(Name = "id")] long id, [FromBody] GroupPermissionType type)
+        => Ok(await this._groupPermissionService.ModifyAsync(id, type));
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync([FromRoute(Name = "id")] long id)
+        => Ok(await this._groupPermissionService.RemoveAsync(id));
+}
