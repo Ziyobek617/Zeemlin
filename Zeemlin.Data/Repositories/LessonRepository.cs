32cdc6c8d262d5591e5ee2bb0d3414710fd49374
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2023-12-19 12:01:56 +0500

    Add: New Lesson Repositories

---
+++ Zeemlin.Data/Repositories/LessonRepository.cs
@@ -0,0 +1,17 @@
+﻿using Zeemlin.Data.DbContexts;
+using Zeemlin.Data.IRepositries;
+using Zeemlin.Domain.Entities;
+
+namespace Zeemlin.Data.Repositories;
+
+public class LessonRepository : Repository<Lesson>, ILessonRepository
+{
+    public LessonRepository(AppDbContext dbContext) : base(dbContext)
+    {
+    }
+
+    public Task<IEnumerable<Lesson>> GetSubscribedLessonsAsync(long Id)
+    {
+        throw new NotImplementedException();
+    }
+}
