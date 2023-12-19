4ae768215a40363bebc6718d5ef6989d5eac034c
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2023-12-19 12:00:10 +0500

    Add: New Lesson entity

---
+++ Zeemlin.Domain/Entities/Lesson.cs
@@ -0,0 +1,16 @@
+﻿using Zeemlin.Domain.Commons;
+
+namespace Zeemlin.Domain.Entities;
+
+public class Lesson : Auditable
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public TimeSpan Duration { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public long GroupId { get; set; }
+    public Group Group { get; set; }
+    public ICollection<Grade> Grades { get; set;}
+    public ICollection<LessonAttendance> LessonAttendances { get;set;}
+}
