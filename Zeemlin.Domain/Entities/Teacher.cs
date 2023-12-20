4ee06ad9b003f1a5dfa098ac5a891604e862b042
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2023-12-21 01:17:15 +0500

    Add: New entity Teacher

---
+++ Zeemlin.Domain/Entities/Teacher.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Zeemlin.Domain.Commons;
+
+namespace Zeemlin.Domain.Entities;
+
+public class Teacher : Auditable
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string DateOfBirth { get; set; }
+    [Phone]
+    public string PhoneNumber { get; set; }
+}
