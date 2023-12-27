a173515620bc2c9fa97a40f7d0e830ee811b1b40
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2023-12-27 20:52:37 +0500

    Added: New enum ScienceType

---
+++ Zeemlin.Domain/Enums/ScienceType.cs
@@ -0,0 +1,23 @@
+﻿namespace Zeemlin.Domain.Enums;
+
+public enum ScienceType
+{
+    Art, // Chizmachilik
+    Music, // Musiqa
+    Physics, // Fizika
+    History, // Jahon Tarixi
+    Biology, // Biologiya
+    Algebra, // Algebra
+    Upbring, // Tarbiya
+    Geometry, // Geometriya
+    Chemistry, // Kimyo
+    Geography, // Geografiya
+    Mathematics, // Matematika
+    UzbekLanguage, // Ona tili
+    GermanLanguage, // Nemis tili
+    FrenchLanguage, // Fransuz tili
+    RussianLanguage, // Rus tili
+    EnglishLanguage, // Ingliz tili
+    HistoryOfUzbekistan, // O'zbekiston Tarixi
+    InformationTechnology, // Informatika
+}
