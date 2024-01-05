2b45c76fe53d84ab9a6220141b99e8e3c41c55b9
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-01-05 21:18:19 +0500

    Update: Changed User in Grade class to Student. UserId changed to TeacherId

--- Zeemlin.Domain/Entities/Grade.cs
+++ Zeemlin.Domain/Entities/Grade.cs
@@ -4,8 +4,8 @@ namespace Zeemlin.Domain.Entities;
 
 public class Grade : Auditable
 {
-    public long UserId { get; set; }
-    public Student User { get; set; } // One-to-many relationship
+    public long StudentId { get; set; }
+    public Student Student { get; set; } // One-to-many relationship
     public long HomeworkId { get; set; }
     public Homework Homework { get; set; } // One-to-many relationship
     public int Value { get; set; }
