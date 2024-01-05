0ecb8864e9830d4f19b33242bc3a2cf4ff4d5af0
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-01-05 22:13:02 +0500

    Add new entities: LessonTable and School

---
+++ Zeemlin.Domain/Entities/LessonTable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Zeemlin.Domain.Commons;
+
+namespace Zeemlin.Domain.Entities;
+
+public class LessonTable : Auditable
+{
+    public DateTime GetDate { get; set; } // What day is the lesson? For example: Monday or Tuesday
+    public string Title { get; set; } // Which science is written here
+
+    public int GroupId { get; set; } // It is written here which group it belongs to
+    public Group Group { get; set; }
+    public int TeacherId { get; set; } // Write here which teacher will teach the group
+    public Teacher Teacher { get; set; }
+
+}
---
+++ Zeemlin.Domain/Entities/School.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using Zeemlin.Domain.Commons;
+using Zeemlin.Domain.Enums;
+
+namespace Zeemlin.Domain.Entities;
+
+public class School : Auditable
+{
+    [Required] // Adjust MaxLength as needed
+    public long SchoolNumber { get; set; }
+
+    [Required]
+    [MaxLength(255)] // Adjust MaxLength as needed
+    public string Name { get; set; }
+    [Required]
+    [MaxLength(2000)]
+    public string Description { get; set; }
+
+    // Address
+    [Required]
+    [MaxLength(50)]
+    public string Country { get; set; }
+    public Region Region { get; set; }
+    [Required]
+    [MaxLength(50)]
+    public string DistrictName { get; set; }
+    [Required]
+    [MaxLength(50)]
+    public string GeneralAddressMFY { get; set; }
+    [Required]
+    [MaxLength(50)]
+    public string StreetName { get; set; }
+
+    // Contact Information
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public string Website { get; set; }
+}
