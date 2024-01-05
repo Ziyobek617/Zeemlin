0eed7d024c7e2c14a7b9e90849711f3f351d4097
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-01-05 22:03:16 +0500

    Added: TeacherId

--- Zeemlin.Domain/Entities/StudentGroup.cs
+++ Zeemlin.Domain/Entities/StudentGroup.cs
@@ -5,9 +5,10 @@ namespace Zeemlin.Domain.Entities;
 
 public class StudentGroup : Auditable
 {
-    public long UserId { get; set; }
-    public Student Users { get; set; }
-
+    public long TeacherId { get; set; }
+    public Teacher Teacher { get; set; }
+    public long StudentId { get; set; }
+    public Student Student { get; set; }
     public long GroupId { get; set; }
     public Group Group { get; set; }
 }
