1e2e6f89717c2bfa1f1d33c045b5a32d62862787
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-01-05 22:18:26 +0500

    Add: Create Region enum

---
+++ Zeemlin.Domain/Enums/Region.cs
@@ -0,0 +1,19 @@
+﻿namespace Zeemlin.Domain.Enums;
+
+public enum Region
+{
+    Tashkent = 1,
+    TashkentCity = 2,
+    Andijan = 3,
+    Namangan = 4,
+    Fergana = 5,
+    Bukhara = 6,
+    Jizzakh = 7,
+    Kashkadarya = 8,
+    Navoiy = 9,
+    Karakalpakstan = 10,
+    Samarkand = 11,
+    Surxondaryo = 12,
+    Syrdarya = 13,
+    Khorezm = 14
+}
