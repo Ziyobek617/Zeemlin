c28589ba8ac829a111e354b25985c4196575bf53
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-01-14 17:01:35 +0500

    Added: SchoolRepository

---
+++ Zeemlin.Data/Repositories/SchoolRepository.cs
@@ -0,0 +1,12 @@
+﻿using Zeemlin.Data.DbContexts;
+using Zeemlin.Data.IRepositries;
+using Zeemlin.Domain.Entities;
+
+namespace Zeemlin.Data.Repositories;
+
+public class SchoolRepository : Repository<School>, ISchoolRepository
+{
+    public SchoolRepository(AppDbContext dbContext) : base(dbContext)
+    {
+    }
+}
