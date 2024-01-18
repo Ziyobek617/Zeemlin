9b1575117c72bb67767884481bd99270f5e09ab7
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-01-18 22:31:42 +0500

    GroupId added for Grade class

--- Zeemlin.Domain/Entities/Grade.cs
+++ Zeemlin.Domain/Entities/Grade.cs
@@ -4,8 +4,10 @@ namespace Zeemlin.Domain.Entities;
 
 public class Grade : Auditable
 {
-    public long StudentId { get; set; }
+    public long UserId { get; set; }
     public Student Student { get; set; } // One-to-many relationship
+    public long GroupId { get; set; }
+    public Group Group { get; set; }
     public long HomeworkId { get; set; }
     public Homework Homework { get; set; } // One-to-many relationship
     public int Value { get; set; }
--- Zeemlin.Domain/Entities/LessonTable.cs
+++ Zeemlin.Domain/Entities/LessonTable.cs
@@ -8,9 +8,9 @@ public class LessonTable : Auditable
     public DateTime GetDate { get; set; } // What day is the lesson? For example: Monday or Tuesday
     public string Title { get; set; } // Which science is written here
 
-    public int GroupId { get; set; } // It is written here which group it belongs to
+    public long GroupId { get; set; } // It is written here which group it belongs to
     public Group Group { get; set; }
-    public int TeacherId { get; set; } // Write here which teacher will teach the group
+    public long TeacherId { get; set; } // Write here which teacher will teach the group
     public Teacher Teacher { get; set; }
 
 }
