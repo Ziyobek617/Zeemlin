d2a118943c80c37c019e41f8d77cc3b37a943861
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-01-22 15:41:00 +0500

    The TeachersController api has been released

---
+++ Zeemlin.Api/Controllers/Users/TeachersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Zeemlin.Service.DTOs.Grade;
+using Zeemlin.Service.DTOs.Teachers;
+using Zeemlin.Service.Interfaces;
+
+namespace Zeemlin.Api.Controllers.Users;
+
+[Authorize]
+public class TeachersController : BaseController
+{
+    private readonly ITeacherService _teacherService;
+
+    public TeachersController(ITeacherService teacherService)
+    {
+        _teacherService = teacherService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync([FromBody] TeacherForCreationDto dto)
+        => Ok(await _teacherService.CreateAsync(dto));
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync()
+        => Ok(await this._teacherService.RetrieveAllAsync());
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetAsync([FromRoute(Name = "id")] long id)
+        => Ok(await this._teacherService.RetrieveByIdAsync(id));
+
+    [HttpDelete("{id:long}")]
+    public async Task<IActionResult> DeleteAsync([FromRoute(Name = "id")] long id)
+        => Ok(await this._teacherService.RemoveAsync(id));
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutAsync([FromRoute(Name = "id")] long id, [FromBody] TeacherForUpdateDto dto)
+        => Ok(await this._teacherService.ModifyAsync(id, dto));
+}
