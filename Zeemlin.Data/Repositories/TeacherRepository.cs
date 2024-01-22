4ed9372319dee61edab186186712e05e099f8d2d
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-01-22 13:27:38 +0500

    ITeacherRepository and TeacherRepository are ready

---
+++ Zeemlin.Data/Repositories/TeacherRepository.cs
@@ -0,0 +1,12 @@
+﻿using Zeemlin.Data.DbContexts;
+using Zeemlin.Data.IRepositries;
+using Zeemlin.Domain.Entities;
+
+namespace Zeemlin.Data.Repositories;
+
+public class TeacherRepository : Repository<Teacher>, ITeacherRepository
+{
+    public TeacherRepository(AppDbContext dbContext) : base(dbContext)
+    {
+    }
+}
