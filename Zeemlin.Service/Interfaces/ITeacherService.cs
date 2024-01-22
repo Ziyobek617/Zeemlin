439f5eddffe3e59d6539315e86d2e53c6559f367
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-01-22 13:14:46 +0500

    Add ITeacherService

---
+++ Zeemlin.Service/Interfaces/ITeacherService.cs
@@ -0,0 +1,13 @@
+﻿using Zeemlin.Service.DTOs.Teachers;
+using Zeemlin.Service.DTOs.UserGroup;
+
+namespace Zeemlin.Service.Interfaces;
+
+public interface ITeacherService
+{
+    Task<bool> RemoveAsync(long id);
+    Task<TeacherForResultDto> RetrieveIdAsync(long id);
+    Task<IEnumerable<TeacherForResultDto>> RetrieveAllAsync();
+    Task<TeacherForResultDto> ModifyAsync(long id, TeacherForUpdateDto dto);
+    Task<TeacherForResultDto> CreateAsync(TeacherForCreationDto dto);
+}
