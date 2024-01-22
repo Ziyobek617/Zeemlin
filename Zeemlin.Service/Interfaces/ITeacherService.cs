61e1093b6fb8f0a4307ae81da2528b9a28e842ca
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-01-22 15:17:46 +0500

    Updated RetrieveById name in ITeacherService

--- Zeemlin.Service/Interfaces/ITeacherService.cs
+++ Zeemlin.Service/Interfaces/ITeacherService.cs
@@ -6,7 +6,7 @@ namespace Zeemlin.Service.Interfaces;
 public interface ITeacherService
 {
     Task<bool> RemoveAsync(long id);
-    Task<TeacherForResultDto> RetrieveIdAsync(long id);
+    Task<TeacherForResultDto> RetrieveByIdAsync(long id);
     Task<IEnumerable<TeacherForResultDto>> RetrieveAllAsync();
     Task<TeacherForResultDto> ModifyAsync(long id, TeacherForUpdateDto dto);
     Task<TeacherForResultDto> CreateAsync(TeacherForCreationDto dto);
