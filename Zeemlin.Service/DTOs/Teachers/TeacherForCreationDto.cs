b8d62c48c55d090139767b5d176e501f9cfbdd9c
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-01-22 12:58:04 +0500

    Add DTOs for Teacher class

---
+++ Zeemlin.Service/DTOs/Teachers/TeacherForCreationDto.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Zeemlin.Service.DTOs.Teachers
+{
+    public class TeacherForCreationDto
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+
+        [PasswordPropertyText]
+        [Required]
+        public string Password { get; set; }
+
+        public string Biography { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string DistrictName { get; set; }
+
+        [Required]
+        public long SchoolNumber { get; set; }
+
+    }
+}
---
+++ Zeemlin.Service/DTOs/Teachers/TeacherForUpdateDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Zeemlin.Service.DTOs.Teachers
+{
+    public class TeacherForUpdateDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; } // Make nullable for optional updates
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [PasswordPropertyText]
+        public string Password { get; set; } // Allow optional password updates
+
+        public string Biography { get; set; }
+
+        public string DistrictName { get; set; } // Allow optional updates
+        public long? SchoolNumber { get; set; }  // Allow optional updates
+    }
+}
