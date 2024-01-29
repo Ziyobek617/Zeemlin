3b086c59541149f78740cb0123407a07f4a989e1
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-01-30 00:45:23 +0500

    Create IStudentGroupService

---
+++ Zeemlin.Service/Interfaces/IStudentGroupService.cs
@@ -0,0 +1,12 @@
+﻿using Zeemlin.Service.DTOs.StudentGroups;
+
+namespace Zeemlin.Service.Interfaces;
+
+public interface IStudentGroupService
+{
+    Task<bool> RemoveAsync(long id);
+    Task<StudentGroupForResultDto> RetrieveByIdAsync(long id);
+    Task<IEnumerable<StudentGroupForResultDto>> RetrieveAllAsync();
+    Task<StudentGroupForResultDto> AddAsync(StudentGroupForCreationDto schoolDto);
+    Task<StudentGroupForResultDto> ModifyAsync(long id, StudentGroupForUpdateDto schoolDto);
+}
