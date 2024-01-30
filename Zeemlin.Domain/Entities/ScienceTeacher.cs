fd03246f220f700fe341d2f8bb185980fe0d5c63
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-01-30 15:40:07 +0500

    Create ScienceTeacher class many-to-many

---
+++ Zeemlin.Domain/Entities/ScienceTeacher.cs
@@ -0,0 +1,12 @@
+﻿using Zeemlin.Domain.Commons;
+
+namespace Zeemlin.Domain.Entities;
+
+public class ScienceTeacher : Auditable
+{
+    public long ScienceId { get; set; }
+    public Science Science { get; set; }
+
+    public long TeacherId { get; set; }
+    public Teacher Teacher { get; set;}
+}
