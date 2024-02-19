fd4b4b5b5a13f95d7995cedfd1ac5f16fd43b903
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-02-19 14:52:31 +0500

    Create AssessmentType enum.

---
+++ Zeemlin.Domain/Enums/AssessmentType.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zeemlin.Domain.Enums
+{
+    public enum AssessmentType
+    {
+        Activity,
+        Classwork,
+        Homework,
+        Projects,
+        Test,
+        FinalExams,
+
+    }
+}
