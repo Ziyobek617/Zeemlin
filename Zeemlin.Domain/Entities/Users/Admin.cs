bbfb40979c92642943db318c1c1deade39af4a77
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-02-21 23:45:47 +0500

    Create Admin and Director classes

---
+++ Zeemlin.Domain/Entities/Users/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Zeemlin.Domain.Commons;
+
+namespace Zeemlin.Domain.Entities.Users;
+
+public class Admin : Auditable
+{
+    public string Username { get; set; } // Username for authentication
+    public string FirstName { get; set; } // Admin's first name
+    public string LastName { get; set; } // Admin's last name
+    public string Email { get; set; } // Admin's email address
+    public string Password { get; set; } // Password for authentication (securely hashed!)
+    public long SchoolId { get; set; }
+    public School School { get; set; }
+
+}
---
+++ Zeemlin.Domain/Entities/Users/Director.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Zeemlin.Domain.Commons;
+
+namespace Zeemlin.Domain.Entities.Users
+{
+    public class Director : Auditable
+    {
+        [Required]
+        public string Username { get; set; } // Username for authentication
+
+        [Required]
+        public string FirstName { get; set; } // Director's first name
+
+        [Required]
+        public string LastName { get; set; } // Director's last name
+
+        [Required]
+        public string Email { get; set; } // Director's email address
+
+        [Required]
+        public string Password { get; set; } // Password for authentication (securely hashed!)
+
+        // List of Director's schools, ensuring bidirectional relationship
+        public ICollection<School> Schools { get; set; } = new HashSet<School>();
+
+        // Method to access a specific school by ID
+        public School GetSchoolByName(string SchoolName)
+        {
+            return Schools.FirstOrDefault(s => s.Name == SchoolName);
+        }
+
+        public Director(string username, string firstName, string lastName, string email, string password)
+        {
+            Username = username;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+        }
+    }
+}
