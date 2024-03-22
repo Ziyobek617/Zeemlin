94484f2aead506e88ae5fe953e9b9d56288e917d
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-03-22 23:25:18 +0500

    Delete Sciences and ScienceTeachers DTOs

--- Zeemlin.Service/DTOs/ScienceTeachers/ScienceTeacherForCreationDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Zeemlin.Service.DTOs.ScienceTeachers;
-
-public class ScienceTeacherForCreationDto
-{
-    public long ScienceId { get; set; }
-    public long TeacherId { get; set; }
-}
--- Zeemlin.Service/DTOs/ScienceTeachers/ScienceTeacherForUpdateDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Zeemlin.Service.DTOs.ScienceTeachers;
-
-public class ScienceTeacherForUpdateDto
-{
-    public long ScienceId { get; set; }
-    public long TeacherId { get; set; }
-}
