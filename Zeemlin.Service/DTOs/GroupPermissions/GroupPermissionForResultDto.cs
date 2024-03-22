56fc2aeff8b5c9487e0707f6ed6eb50890d16738
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-03-22 23:23:49 +0500

    Delete GroupPermissionType enum and DTOs

--- Zeemlin.Service/DTOs/GroupPermissions/GroupPermissionForResultDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using Zeemlin.Domain.Enums;
-
-namespace Zeemlin.Service.DTOs.GroupPermissions;
-
-public class GroupPermissionForResultDto
-{
-    public long Id { get; set; }
-    public GroupPermissionType Type { get; set; }
-}
--- Zeemlin.Service/DTOs/GroupRole/GroupRoleForResultDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using Zeemlin.Domain.Enums;
-
-namespace Zeemlin.Service.DTOs.GroupRole;
-
-public class GroupRoleForResultDto
-{
-    public long Id { get; set; }
-    public Role Role { get; set; }
-}
