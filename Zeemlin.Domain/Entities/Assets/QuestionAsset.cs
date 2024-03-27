6a7df2b8f3604bb759122f4ee4468dd0f1c69bd0
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-03-27 12:57:23 +0500

    Create Question Asset class

---
+++ Zeemlin.Domain/Entities/Assets/QuestionAsset.cs
@@ -0,0 +1,12 @@
+﻿using Zeemlin.Domain.Commons;
+using Zeemlin.Domain.Entities.Questions;
+
+namespace Zeemlin.Domain.Entities.Assets;
+
+public class QuestionAsset : Auditable
+{
+    public string Path { get; set; }
+    public DateTime UploadedDate { get; set; }
+    public long QuestionId { get; set; }
+    public Question Question { get; set; }
+}
